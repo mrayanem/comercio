773a2e6bb08630d339f3bb44b95b19952f101f84
Rayane Marques <mmarquesraioane586@gmail.com>
2023-06-15 08:17:30 -0300

    início controle de versão

---
+++ src/main/java/com/itb/inf2gm/comercio/model/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.inf2gm.comercio.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarra;
+	private double preco;
+	
+	// public: acesso liberado para todas as classes.
+	// private: acesso permitido apenas para os membros 
+	// da própria classe.
+	// endende-se por membros (atributos e ou métodos).
+
+}
