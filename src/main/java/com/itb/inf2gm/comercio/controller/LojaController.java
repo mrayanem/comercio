9d276d91b9b3c9ffe613671db51002e80ae53daa
Rayane Marques <mmarquesraioane586@gmail.com>
2023-08-08 09:56:11 -0300

    att

--- src/main/java/com/itb/inf2gm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2gm/comercio/controller/LojaController.java
@@ -1,16 +1,53 @@
 package com.itb.inf2gm.comercio.controller;
 
+ 
+
+import java.util.ArrayList;
+import java.util.List;
+
+ 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+ 
+
+import com.itb.inf2gm.comercio.model.Produto;
+
+ 
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
-	
-	@GetMapping("/listar")
-	public String listarProdutos() {
-		return "produtos";
-	}
 
-}
+    List<Produto> listaDeProdutos = new ArrayList<Produto>();
+
+        @GetMapping("/listar")
+        public String listarProdutos(Model model) {
+
+
+            //passando a lista de produtos para o front-end
+            model.addAttribute("ListaDeProdutos", listaDeProdutos);
+            return "produtos";
+        }
+
+ 
+
+        @GetMapping("/novo-produto")
+        public String novoProduto(Model model, Produto produto) {
+
+
+            model.addAttribute("produto", produto);
+                return "novo-prod";
+                }
+
+        @PostMapping("add/produto")
+        public String gravarNovoProduto(Model model, Produto produto) {
+
+            listaDeProdutos.add(produto);
+            return "redirect:/comercio/produtos/listar";
+        }
+    }
--- src/main/java/com/itb/inf2gm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2gm/comercio/model/Produto.java
@@ -1,25 +1,53 @@
 package com.itb.inf2gm.comercio.model;
 
-public class Produto {
-	
-	private Long id;
-	private String nome;
-	private String descricao;
-	private String codigoBarra;
-	private double preco;
-	
-	// public: acesso liberado para todas as classes.
-	// private: acesso permitido apenas para os membros 
-	// da própria classe.
-	// endende-se por membros (atributos e ou métodos).
+ 
 
-	
-	public void setId(Long id) {
-		this.id= id; 
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String codigoBarra;
+    private double preco;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getCodigoBarra() {
+        return codigoBarra;
+    }
+    public void setCodigoBarra(String codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    // public: acesso liberado para todas as classes.
+    // private: acesso permitido apenas para os membros da própria classe.
+    // entende-se por membros (atributos e ou métodos)
+
+    // métodos setter's e getter são responsáveis em atribuir e recuperar informações
+    //dos atributos "RESPECTIVAMENTE"
+
+ 
+
+    
 }
